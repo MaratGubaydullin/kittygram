81021f28f5bf5314b63225524c1e6bc5c64377f2
mgubaydullin <mgubaydullin@131.ru>
2024-11-16 16:20:52 +0300

    main first_step

--- cats/views.py
+++ cats/views.py
@@ -9,11 +9,26 @@ from .serializers import CatSerializer
 @api_view(['GET', 'POST'])
 def cat_list(request):
     if request.method == 'POST':
-        serializer = CatSerializer(data=request.data)
+        # Создаём объект сериализатора
+        # и передаём в него данные из POST-запроса
+        serializer = CatSerializer(data=request.data, many=False)
+        # many=True, если надо обработать список объектов
+        # если стоит many=True то отдельные оъекты не воспринимаются
+        # вернется "Expected a list of items but got type dict."
         if serializer.is_valid():
+            # Если полученные данные валидны —
+            # сохраняем данные в базу через save().
             serializer.save()
+            # Возвращаем JSON со всеми данными нового объекта
+            # и статус-код 201
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+        # Если данные не прошли валидацию —
+        # возвращаем информацию об ошибках и соответствующий статус-код:
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    # Получаем все объекты модели
     cats = Cat.objects.all()
-    serializer = CatSerializer(cats, many=True)
+    # Передаём queryset в конструктор сериализатора
+    serializer = CatSerializer(cats, many=True,)
+    # Если при обработке GET-запроса в сериализаторе не указать параметр
+    # many=True, вернётся ошибка AttributeError.
     return Response(serializer.data)
